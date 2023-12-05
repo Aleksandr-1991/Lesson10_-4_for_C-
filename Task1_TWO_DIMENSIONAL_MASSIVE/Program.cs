0084dcbffac18919c8fbc4af7c26f012f7dee761
Aleks37 <lao35180972643335589007>
2023-12-05 22:03:24 +0400

    Commit from 05.12.23

---
+++ Task1_TWO_DIMENSIONAL_MASSIVE/Program.cs
@@ -0,0 +1,44 @@
+﻿//(блок) 
+/*
+string [,] table = new string [2, 5];
+// table[0,0]  table[0,1]  table[0,2]  table[0,3]  table[0,4]
+// table[1,0]  table[1,1]  table[1,2]  table[1,3]  table[1,4]
+// String.Empty //- помним что так объявляется пустая строка.
+table[1, 2] = "Слово"; 
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.Write($"~{table[rows, columns]}~ ");
+    }
+Console.WriteLine();
+}                            //*/
+
+//(блок) /*
+int [,] matrix = new int [3, 4];
+void PrintArray(int [,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)  // .GetLength(0) - возвращает кол-во строк массива.
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // .GetLength(1) - возвращает кол-во столбцов.
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);                                         //*/
---
+++ Task3_FIBONACCI/Program.cs
@@ -0,0 +1,7 @@
+﻿double Fibonacci (int n)
+{   
+    // f(1)= 1;  f(2)= 1;   f(n) = f(n-1) + f(n-2) 
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i=1; i < 40; i++) Console.WriteLine($"{i}-й член послед-ти Фибоначчи = {Fibonacci (i)}.");
